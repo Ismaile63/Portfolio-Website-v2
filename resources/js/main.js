0d770ce01365c85fc777f30666a2f6c7f1a4957a
Ismaile <ismaile63@gmail.com>
2025-02-22 20:17:41 +0000

    About and Projects QOL. Home animation

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,80 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('html'); 
+    const backgrounds = ['#0d103c', '#fffee7', '#191c4b', '#3e3f4e'];
+    const largeNums = [60, 65, 70, 80]; 
+    const smallNums = [8, 9, 10, 11];
+    
+    const coordinates = {
+        x: undefined, 
+        y: undefined 
+    }
+
+    const generateNumber = () => { 
+        return Math.floor(Math.random() * 4);
+    }
+
+    const createRandomNumbers = () => {
+        const width = generateNumber(); 
+        const height = generateNumber(); 
+        const color = generateNumber(); 
+        return [width, height, color]; 
+    }
+
+    const configureWideElement = (newEle) => {
+        const [width, height, color] = createRandomNumbers(); 
+        newEle.style.width = largeNums[width] + 'px'; 
+        newEle.style.height = smallNums[height] + 'px'; 
+        newEle.style.backgroundColor = backgrounds[color];
+        newEle.classList.add('line', 'line-wide');
+        
+    }
+
+    const configureTallElement = (newEle) => {
+        const [width, height, color] = createRandomNumbers(); 
+        newEle.style.width = smallNums[width] + 'px'; 
+        newEle.style.height = largeNums[height] + 'px'; 
+        newEle.style.backgroundColor = backgrounds[color];
+        newEle.classList.add('line', 'line-high');
+
+    }
+
+    const addElement = () => {
+        const newEle = document.createElement('div');
+        const typeNum = Math.round(Math.random()); //returns 0 or 1;  
+
+        if(typeNum === 0) { 
+            configureWideElement(newEle);
+        } else {
+            configureTallElement(newEle); 
+        }
+
+        console.log(newEle); 
+
+        newEle.style.left = coordinates.x + 'px'; 
+        newEle.style.top = coordinates.y + 'px'; 
+        header.appendChild(newEle); 
+
+    }
+
+    const updateCoordinates = e => { 
+        if(coordinates.x === undefined || coordinates.y === undefined){
+            coordinates.x = e.x; 
+            coordinates.y = e.y;
+            addElement(); 
+        }
+
+        if(Math.abs(coordinates.x - e.x) > 50 || Math.abs(coordinates.y - e.y) > 50) {
+            coordinates.x = e.x; 
+            coordinates.y = e.y;
+            addElement(); 
+        } 
+    }
+
+    header.addEventListener('mousemove', e => {
+        updateCoordinates(e); 
+    });
+}); 
+
 window.onload = function () {
     window.addEventListener('scroll', function (e) {
         if (window.pageYOffset > 100) {
